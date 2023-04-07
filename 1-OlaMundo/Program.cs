f75b72327b341bbc02262b0edf39931fad160732
Jeff <93086727+Jeffersfaria@users.noreply.github.com>
2023-04-06 22:38:03 -0300

    Add files via upload

---
+++ 1-OlaMundo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, mundo!");
+        Console.WriteLine("Tecle enter para continuar...");
+        Console.ReadLine();
+    }
+}
+
+
+
