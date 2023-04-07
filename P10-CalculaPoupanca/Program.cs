3420ec6567f0796d6e0cb54ebbf0f1dd43a51b66
Jeff <93086727+Jeffersfaria@users.noreply.github.com>
2023-04-06 22:44:09 -0300

    Add files via upload

---
+++ P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 10");
+
+        double investimento = 1000;
+
+        // rendimento de 0.5% ao mês (0.005)
+
+        //investimento += investimento * 0.005;
+        
+        //Console.WriteLine(investimento);
+
+        int mes = 1;
+        while (mes <= 12) 
+        {
+            investimento += investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " você tem R$" + investimento);
+            mes++;
+        }
+
+
+
+        Console.WriteLine("Tecle enter para continuar...");
+        Console.ReadLine();
+    }
+}
